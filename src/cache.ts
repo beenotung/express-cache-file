4d57ce1b7ca547ccab5a504fa39256e42c5d740b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-02-06 18:25:40 +0800

    implemented logic to maintain bounded cache size

--- src/cache.ts
+++ src/cache.ts
@@ -1,4 +1,4 @@
-import { Cache, Callback, GetFileFn } from './types';
+import { Cache, Callback, Content, GetFileFn } from './types';
 import parseInterval, { UpdateInterval } from './interval';
 
 export type UpdateOptions = {
@@ -27,16 +27,14 @@ function getCacheFile(
       if (mode === 'cache_first') {
         cb(null, item);
       }
-      delete cache.files[file];
-      cache.unsedSize -= item.buffer.byteLength;
+      removeFromCache(cache, file, item);
     }
     getFile(file, (err, content) => {
       if (!err) {
         if (expire >= 0) {
           content.expire = Date.now() + expire;
         }
-        cache.files[file] = content;
-        cache.unsedSize += content.buffer.byteLength;
+        saveToCache(cache, file, content);
         // TODO auto remove other files when cache is too large
       }
       if (!item || mode !== 'cache_first') {
@@ -44,6 +42,40 @@ function getCacheFile(
       }
     });
   };
+}
+
+function removeFromCache(cache: Cache, file: string, item = cache.files[file]) {
+  delete cache.files[file];
+  if (item) {
+    cache.unsedSize -= item.buffer.byteLength;
+  }
+}
+
+function saveToCache(cache: Cache, file: string, content: Content) {
+  removeFromCache(cache, file);
+  if (checkCacheSize(cache, content.buffer.byteLength)) {
+    cache.files[file] = content;
+    cache.unsedSize += content.buffer.byteLength;
+  }
+}
+
+function checkCacheSize(cache: Cache, sizeNeeded: number): boolean {
+  if (!cache.capacity) return true;
+
+  if (cache.capacity >= cache.unsedSize + sizeNeeded) {
+    return true;
+  }
+
+  const entries = Object.entries(cache.files);
+  for (let i = 0; i < entries.length; i++) {
+    if (cache.capacity >= cache.unsedSize + sizeNeeded) {
+      return true;
+    }
+    let entry = entries[i];
+    removeFromCache(cache, entry[0], entry[1]);
+  }
+
+  return cache.capacity >= cache.unsedSize + sizeNeeded;
 }
 
 export default getCacheFile;
