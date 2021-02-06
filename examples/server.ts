4eec621adb9ad4cdc6b202a41e034b439328b2aa
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-02-06 18:26:09 +0800

    added links to example page

--- examples/server.ts
+++ examples/server.ts
@@ -4,17 +4,32 @@ import cacheFile from '../src';
 
 let app = express();
 
+// blacklist
 let rejectFile = (req: Request, res: Response) => {
-	res.status(403).end(`${req.method} ${req.originalUrl} 403 Forbidden`);
+  res.status(403).end(`${req.method} ${req.originalUrl} 403 Forbidden`);
 };
 app.use('/*.gitignore', rejectFile);
 app.use('/*.git/*', rejectFile);
 
-app.use(cacheFile(join(__dirname, 'public')));
+// serve static files
+let root = join(__dirname, 'public');
+app.use(
+  cacheFile(root, {
+    cacheSize: '50mb',
+    update: {
+      expire: '5 seconds',
+      mode: 'cache_first',
+    },
+  }),
+);
+
+// apis
 app.get('/user', (req, res) => res.json({ type: 'from api' }));
-app.get('/user/:id', (req, res) => res.json({ params: req.params, query: req.query }));
+app.get('/user/:id', (req, res) =>
+  res.json({ params: req.params, query: req.query }),
+);
 
 let port = process.env.PORT || 8100;
 app.listen(port, () => {
-	console.log('listening on http://localhost:' + port);
+  console.log('listening on http://localhost:' + port);
 });
